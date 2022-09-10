eee189c9a05e5246567126f1d6f6321bf278d93f
Daniel Doyle <tr33m4n@googlemail.com>
2022-09-10 11:34:35 +0100

    Ensure module supports both v4 and v5 versions of Codeception

--- rector.php
+++ rector.php
@@ -3,11 +3,8 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Set\ValueObject\SetList;
 use Rector\TypeDeclaration\Rector\FunctionLike\ReturnTypeDeclarationRector;
-use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
-use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(SetList::DEAD_CODE);
@@ -20,18 +17,8 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([__DIR__ . '/src']);
     $rectorConfig->skip([
         ReturnTypeDeclarationRector::class => [
-            __DIR__ . '/src/Codeception/Module/Percy/Exchange/Adapter/CurlAdapter.php',
             __DIR__ . '/src/Codeception/Module/Percy/RequestManagement.php',
             __DIR__ . '/src/Codeception/Module/Percy/Snapshot.php'
-        ],
-        TypedPropertyRector::class => [
-            __DIR__ . '/src/Codeception/Module/Percy/Exchange/Adapter/CurlAdapter.php'
-        ],
-        TypedPropertyFromAssignsRector::class => [
-            __DIR__ . '/src/Codeception/Module/Percy/Exchange/Adapter/CurlAdapter.php'
-        ],
-        TypedPropertyFromStrictConstructorRector::class => [
-            __DIR__ . '/src/Codeception/Module/Percy/Exchange/Adapter/CurlAdapter.php'
         ]
     ]);
 };
